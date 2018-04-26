888421685cc4cbe6b93ac37bd833bbfc67fab228
William Robertson <williamp.robertson@gmail.com>
2018-04-26 02:02:06 -0700

    added firebase creds

--- config/auth/firebase_info_real.js
+++ config/auth/firebase_info_real.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "******************************",
-  authDomain: "**************.firebaseapp.com",
-  databaseURL: "https://*************.firebaseio.com",
-  projectId: "***************",
-  storageBucket: "",
-  messagingSenderId: "***************"
+  apiKey: "AIzaSyDIg4hUrisNLK46jy_5WSs9XFwsOOeUW3Q",
+  authDomain: "employee-check-6a85d.firebaseapp.com",
+  databaseURL: "https://employee-check-6a85d.firebaseio.com",
+  projectId: "employee-check-6a85d",
+  storageBucket: "employee-check-6a85d.appspot.com",
+  messagingSenderId: "690466885001"
 };
